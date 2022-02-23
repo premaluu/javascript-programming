e45ede7ac802b28bc9dd976159c2540b75e9fa14
premalupadhyay_crest <premalupadhyay56@gmail.com>
2022-02-23 22:08:38 +0530

    JS variables and datatypes best practices

---
+++ signup-app.js
@@ -0,0 +1,22 @@
+/**
+ * This file contain sign up app to demonstrate the use of variables and datatype.
+ * 
+ * @author premaluu
+ * @since 23 Feb 2022
+ */
+
+var fullName = "Premal Upadhyay";
+var email = "premalupadhyay56@gmail.com";
+var password = "premal123";
+var confirmPassword = "premal123";
+var isLoggedInFromGoogle = false;
+var courseCount = 0;
+
+console.log(`
+    Full name : ${fullName}
+    Email : ${email}
+    Password : ${password}
+    Confirm password : ${confirmPassword}
+    User is ${isLoggedInFromGoogle} from google.
+    User has taken ${courseCount} courses.
+`);
