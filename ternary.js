f388a504d9fd87173c5595ebccdd942cc7e4c998
premalupadhyay_crest <premalupadhyay56@gmail.com>
2022-02-24 15:50:19 +0530

    ternary operatos in JS

---
+++ ternary.js
@@ -0,0 +1,10 @@
+/**
+ * This file contain sign up app to demonstrate the use of variables and datatype.
+ * 
+ * @author premaluu
+ * @since 24 Feb 2022
+ */
+
+var authenticated = false;
+
+authenticated ? console.log("Signout Button") : console.log("LOGIN");
